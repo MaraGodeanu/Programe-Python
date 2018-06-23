fb7d015e58e8a44c956327080a4dbcc0faf2ae44
MaraGodeanu <40514925+MaraGodeanu@users.noreply.github.com>
2018-06-23 13:04:39 +0300

    Gravity is a thing

--- MariosWorld.py
+++ MariosWorld.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Mario's World")
 clock=pygame.time.Clock()
 
 PINK=(220,40,166)
+jumping=pygame.transform.scale(pygame.image.load("images/mario/mario6.png").convert_alpha(),(32,64))
 standing=pygame.transform.scale(pygame.image.load("images/mario/mario1.png").convert_alpha(),(32,64))
 walking=[pygame.transform.scale(pygame.image.load("images/mario/mario2.png").convert_alpha(),(32,64)), 
          pygame.transform.scale(pygame.image.load("images/mario/mario3.png").convert_alpha(),(32,64)),
@@ -18,6 +19,7 @@ marioy=500
 imagemario=0
 direction=0
 moving=False
+flying=False
 
 while running:
     
@@ -36,26 +38,51 @@ while running:
             imagemario=(imagemario+1)%3
             direction=0
             moving=True
-            
+            flying=False
             
     elif keys[pygame.K_LEFT]:
             mariox=mariox - 10
             imagemario=(imagemario - 1)%3
             direction=1
             moving=True
-    else:
-            moving=False        
+            flying=False
             
+    elif keys[pygame.K_UP]:
+            marioy=marioy - 20
+            flying=True
+            
+    else:
+            moving=False     
+            
+            if marioy>=500:    
+                flying=False
             
             
     screen.fill(PINK)
-    if moving:
+    if moving and not flying:
         if direction==1:
             screen.blit(pygame.transform.flip(walking[imagemario],True,False),(mariox,marioy))     
         else:
             screen.blit(walking[imagemario],(mariox,marioy))
-    else:
+            
+    elif moving and flying :
+         if direction==1:
+             screen.blit(pygame.transform.flip(jumping,True,False),(mariox,marioy))
+         
+         else:   
+             screen.blit(jumping,(mariox,marioy))
+             
+             
+    elif flying:
+        screen.blit(jumping,(mariox,marioy))    
+        if marioy<500:
+            marioy=marioy+10
         
+        
+    else:
+        if marioy<500:
+            marioy=marioy+10
+            
         screen.blit(standing,(mariox,marioy))
     pygame.display.flip()        
     clock.tick(25)        
