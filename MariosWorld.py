f3e1f487154e7ab6e98e31e36d3498e4ff3d863f
MaraGodeanu <40514925+MaraGodeanu@users.noreply.github.com>
2018-06-23 12:12:10 +0300

    Now Mario can actually take a break

--- MariosWorld.py
+++ MariosWorld.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("Mario's World")
 clock=pygame.time.Clock()
 
 PINK=(220,40,166)
+standing=pygame.transform.scale(pygame.image.load("images/mario/mario1.png").convert_alpha(),(32,64))
 walking=[pygame.transform.scale(pygame.image.load("images/mario/mario2.png").convert_alpha(),(32,64)), 
          pygame.transform.scale(pygame.image.load("images/mario/mario3.png").convert_alpha(),(32,64)),
          pygame.transform.scale(pygame.image.load("images/mario/mario4.png").convert_alpha(),(32,64))]
@@ -16,6 +17,7 @@ mariox=500
 marioy=500
 imagemario=0
 direction=0
+moving=False
 
 while running:
     
@@ -33,23 +35,28 @@ while running:
             mariox=mariox + 10
             imagemario=(imagemario+1)%3
             direction=0
+            moving=True
             
-    if keys[pygame.K_LEFT]:
+            
+    elif keys[pygame.K_LEFT]:
             mariox=mariox - 10
             imagemario=(imagemario - 1)%3
             direction=1
+            moving=True
+    else:
+            moving=False        
             
             
             
     screen.fill(PINK)
-    
-    if direction==1:
-        screen.blit(pygame.transform.flip(walking[imagemario],True,False),(mariox,marioy))
-        
+    if moving:
+        if direction==1:
+            screen.blit(pygame.transform.flip(walking[imagemario],True,False),(mariox,marioy))     
+        else:
+            screen.blit(walking[imagemario],(mariox,marioy))
     else:
         
-        screen.blit(walking[imagemario],(mariox,marioy))
-   
+        screen.blit(standing,(mariox,marioy))
     pygame.display.flip()        
     clock.tick(25)        
     
